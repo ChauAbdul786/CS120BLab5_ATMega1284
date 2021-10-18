3aa15925c219ebd76ac740008de84918afec5fe0
Abdullah Chaudhry <56360062+ChauAbdul786@users.noreply.github.com>
2021-10-17 23:16:17 -0700

    Changed Port B to Port C on Part 1

--- turnin/achau048_lab5_part1.c
+++ turnin/achau048_lab5_part1.c
@@ -15,11 +15,11 @@
 int main(void) {
 	/* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
 
 	/* Insert your solution below */
 	unsigned char tmpA = 0x00;
-	unsigned char tmpB = 0x00;
+	unsigned char tmpC = 0x00;
 
 	while (1) {
 		tmpA = PINA;
@@ -27,41 +27,41 @@ int main(void) {
 
 		switch(tmpALower4Bits){
 		case 0x00:
-			tmpB = 0x40; //Fuel Level 0 => 0100 000
+			tmpC = 0x40; //Fuel Level 0 => 0100 000
 					//Not described in lab spec, assuming this is the intended behavior 
 			break;
 		case 0x01:
 		case 0x02:
-			tmpB = 0x60; //Fuel Level 1 or 2 => 0110 0000
+			tmpC = 0x60; //Fuel Level 1 or 2 => 0110 0000
 			break;
 		case 0x03:
 		case 0x04:
-			tmpB = 0x70;	//Fuel Level 3 or 4 => 0111 0000
+			tmpC = 0x70;	//Fuel Level 3 or 4 => 0111 0000
 			break;
 		case 0x05:
 		case 0x06:
-			tmpB = 0x38; //Fuel Level 5 or 6 => 0011 1000
+			tmpC = 0x38; //Fuel Level 5 or 6 => 0011 1000
 			break;
 		case 0x07:
 		case 0x08:
 		case 0x09:
-			tmpB = 0x3C; //Fuel Level 7 - 9 => 0011 1100
+			tmpC = 0x3C; //Fuel Level 7 - 9 => 0011 1100
 			break;
 		case 0x0A:
 		case 0x0B:
 		case 0x0C:
-			tmpB = 0x3E; //Fuel Level 10 - 12 => 0011 1110
+			tmpC = 0x3E; //Fuel Level 10 - 12 => 0011 1110
 			break;
 		case 0x0D:
 		case 0x0E:
 		case 0x0F:
-			tmpB = 0x3F; //Fuel Level 13 - 15 => 0011 1111
+			tmpC = 0x3F; //Fuel Level 13 - 15 => 0011 1111
 		default:
-			tmpB = 0x3F; //If none of these cases, 
+			tmpC = 0x3F; //If none of these cases, 
 					//Fuel Level was likely mistakenly set > 15 => 0011 1111
 			break;
 		}
-	PORTB = tmpB;
+	PORTB = tmpC;
 		
 	}
 	return 1;
